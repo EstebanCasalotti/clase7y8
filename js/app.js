5a838f80d3f15d795be9732bbdea789760afba19
Esteban Casalotti <estebancasalotti2@gmail.com>
2021-08-21 16:56:56 -0300

    cambios finales

--- js/app.js
+++ js/app.js
@@ -1,37 +1,58 @@
-class usuarios {
-    constructor(usuario, email, contraseña){
-        this.usuario = usuario;
-        this.email = email;
-        this.contraseña = contraseña;
+class auto {
+    constructor(marca, color, modelo, precio, vendedor){
+        this.marca = marca;
+        this.color = color;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.vendedor = vendedor;
     }
-}
-const listaConUsuarios = []
-const datos = () => {
-    let usuario = prompt("ingrese su nombre de usuario")
-    let email = prompt("ingrese su email")
-    let contraseña = prompt("ingrese su contraseña")
-    const usuario0 = new usuarios(usuario, email, contraseña)
-    listaConUsuarios.push(usuario0)
-    console.log(listaConUsuarios);
-}
-// para crear mas usuarios escriba en la consola " datos() "
-datos()
+} 
+let vendedor0 = "matias"
+let vendedor1 = "maximiliano"
 
-// para ver los datos escrba en la consola " mostrarDatos() "
-const mostrarDatos = () => {
+let carrito = []
 
-    listaConUsuarios.forEach(elements => {
-        console.log("tu usuario" + " ",elements.usuario);
-        
-    })
+let auto0 = new auto ("bmw", "rojo", "basico", 100000, vendedor0);
+let auto1 = new auto ("ford", "negro", "premium", 600000, vendedor1);
+let auto2 = new auto ("audi", "azul", "premium", 500000, vendedor1);
+let auto3 = new auto ("peugeot", "blanco", "intermedio", 20000, vendedor0);
+let auto4 = new auto ("citroen", "blanco", "basico", 10000, vendedor0);
+let auto5 = new auto ("peugeot", "premium", "clase A", 700000, vendedor1);
+let auto6 = new auto ("bmw", "rojo", "intermedio", 300000, vendedor1);
 
-    listaConUsuarios.forEach(elements => {
-        console.log("tu email es" + " ",elements.email);
-        
-    })
+carrito.push(auto0)
+carrito.push(auto1)
+carrito.push(auto2)
+carrito.push(auto3)
+carrito.push(auto4)
+carrito.push(auto5)
+carrito.push(auto6)
 
-    listaConUsuarios.forEach(elements => {
-        console.log("tu contraseña es" + " ",elements.contraseña);
-        
-    })
-}
+//filtro por marca
+let autosMatias = carrito.filter(e => e.vendedor == vendedor0) 
+console.log(autosMatias);
+
+let autosMaximiliano = carrito.filter(e => e.vendedor == vendedor1)
+console.log(autosMaximiliano);
+
+let autosDeMatias = document.querySelector("#autosDeMatias")
+let autosDeMaximiliano = document.querySelector("#autosDeMaximiliano")
+
+carrito.forEach(e => {
+    autosDeMatias.innerHTML += `
+    <p>${e.marca}</p> 
+    <p>${e.color}</p>
+    <p>${e.modelo}</p>
+    <p>${e.precio}</p>
+    <p>${e.vendedor}</p>
+    `
+})
+carrito.forEach(e => {
+    autosDeMaximiliano.innerHTML += `
+    <p>${e.marca}</p> 
+    <p>${e.color}</p>
+    <p>${e.modelo}</p>
+    <p>${e.precio}</p>
+    <p>${e.vendedor}</p>
+    `
+})
